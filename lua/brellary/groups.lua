411898439eece6c38ee6e24dd214e9ccba8709b8
bavajitu <bavajithuarchlinux@gmail.com>
2026-04-30 17:44:10 +0530

    Major groups configuration

--- lua/brellary/groups.lua
+++ lua/brellary/groups.lua
@@ -3,12 +3,35 @@ local M = {}
 function M.setup(p)
 	local set = vim.api.nvim_set_hl
 
+	-- Tabs and Windows
+	set(0, "TabLine", { fg = p.fg_muted, bg = "none" })
+	set(0, "TabLineSel", { fg = p.fg, bg = "none", bold = true })
+	set(0, "TabLineFill", { bg = "none" })
+
+	-- Popup menus (completion, telescope)
+	set(0, "Pmenu", { fg = p.fg, bg = "none" })
+	set(0, "PmenuSel", { bg = p.selection })
+	set(0, "PmenuThumb", { bg = p.blue })
+
+	-- Search
+	set(0, "Search", { bg = p.yellow, fg = "none" })
+	set(0, "IncSearch", { bg = p.orange, fg = "none" })
+
+	-- Matching brackets
+	set(0, "MatchParen", { bg = p.selection, bold = true })
+
+	-- Git signs (if using gitsigns.nvim)
+	set(0, "DiffAdd", { fg = p.green })
+	set(0, "DiffChange", { fg = p.blue })
+	set(0, "DiffDelete", { fg = p.red })
+	set(0, "SignColumn", { bg = "none" }) -- keeps it transparent
+
 	-- Core UI
 	set(0, "Normal", { fg = p.fg, bg = "none" })
 	set(0, "NormalFloat", { fg = p.fg, bg = "none" })
 	set(0, "FloatBorder", { fg = p.fg_muted, bg = "none" })
 
-	set(0, "CursorLine", { bg = "none" })
+	set(0, "CursorLine", { bg = p.selection, blend = 10 })
 	set(0, "CursorLineNr", { fg = p.fg, bg = "none" })
 	set(0, "LineNr", { fg = p.fg_muted, bg = "none" })
 
@@ -43,16 +66,20 @@ function M.setup(p)
 	set(0, "Operator", { fg = p.fg_dim })
 
 	-- Diagnostics
-	set(0, "DiagnosticError", { fg = p.red })
-	set(0, "DiagnosticWarn", { fg = p.orange })
-	set(0, "DiagnosticInfo", { fg = p.blue })
-	set(0, "DiagnosticHint", { fg = p.green })
+	set(0, "DiagnosticError", { fg = p.red, undercurl = true })
+	set(0, "DiagnosticWarn", { fg = p.orange, undercurl = true })
+	set(0, "DiagnosticInfo", { fg = p.blue, undercurl = true })
+	set(0, "DiagnosticHint", { fg = p.green, undercurl = true })
 
 	-- Treesitter (extended C/C++)
 	set(0, "@variable", { fg = p.fg })
 	set(0, "@variable.parameter", { fg = p.fg_dim })
 	set(0, "@variable.member", { fg = p.fg_dim })
-
+	set(0, "@parameter.reference", { fg = p.fg_dim })
+	set(0, "@keyword.storage", { fg = p.blue }) -- static, const, etc.
+	set(0, "@keyword.modifier", { fg = p.blue }) -- constexpr, noexcept
+	set(0, "@type.qualifier", { fg = p.purple }) -- const, volatile
+	set(0, "@annotation", { fg = p.yellow }) -- [[attributes]]
 	set(0, "@function", { fg = p.blue })
 	set(0, "@function.call", { fg = p.yellow })
 	set(0, "@function.builtin", { fg = p.blue })
@@ -109,6 +136,11 @@ function M.setup(p)
 	set(0, "@markup.raw.block", { fg = p.orange })
 
 	set(0, "@markup.strikethrough", { strikethrough = true })
+
+	-- LSP
+	set(0, "LspReferenceText", { bg = p.selection })
+	set(0, "LspReferenceRead", { bg = p.selection })
+	set(0, "LspReferenceWrite", { bg = p.selection })
 end
 
 return M
