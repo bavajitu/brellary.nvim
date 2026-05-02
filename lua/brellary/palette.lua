e6f8224e1dc94ff375a20f75d37a12a5d2282dc6
bavajitu <bavajithuarchlinux@gmail.com>
2026-05-02 10:45:03 +0530

    Palette changes - mutation

--- lua/brellary/palette.lua
+++ lua/brellary/palette.lua
@@ -4,23 +4,23 @@ function M.get()
 	return {
 		bg = "none",
 
-		-- foreground hierarchy (compressed contrast)
-		fg = "#c6c6c6",
-		fg_dim = "#8a8a8a",
-		fg_muted = "#5c5c5c",
+		-- foreground hierarchy (further reduced contrast)
+		fg = "#b8b8b8",
+		fg_dim = "#7a7a7a",
+		fg_muted = "#4f4f4f",
 
-		-- fully muted accents (reduced saturation + lowered brightness)
-		red = "#a06a6a",
-		orange = "#9a7a5f",
-		yellow = "#a39a5f",
-		green = "#6f8f6f",
-		cyan = "#6f8f8f",
-		blue = "#6f7f9a",
-		purple = "#857a9a",
+		-- accents (more desaturated, darker, less chroma)
+		red = "#8f5f5f",
+		orange = "#8a6a52",
+		yellow = "#8f8752",
+		green = "#5f7a5f",
+		cyan = "#5f7a7a",
+		blue = "#5f6f8a",
+		purple = "#786f8a",
 
-		-- background elements
-		comment = "#4a4d52", -- pushed further into background
-		selection = "#2f2f2f", -- less glare, still visible
+		-- background elements (pushed deeper)
+		comment = "#3f4246",
+		selection = "#262626",
 	}
 end
 
